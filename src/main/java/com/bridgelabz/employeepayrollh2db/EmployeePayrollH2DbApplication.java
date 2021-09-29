06b333d9eb885b7c1cb867ef3083e3135c03b0e2
Prakash Goud <prakash.goud@morningstar.com>
2021-09-30 00:14:06 +0530

    [Prakash] - Created an Employee payroll spring project

---
+++ src/main/java/com/bridgelabz/employeepayrollh2db/EmployeePayrollH2DbApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollh2db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollH2DbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/employeepayrollh2db/EmployeePayrollH2DbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollh2db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollH2DbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
