daf644c4e34f891dc914d689d354ccfb05670169
Prakash Goud <prakash.goud@morningstar.com>
2021-10-01 15:07:54 +0530

    [Prakash] - Added lombok library for logging

--- src/main/java/com/bridgelabz/employeepayrollh2db/EmployeePayrollH2DbApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollh2db/EmployeePayrollH2DbApplication.java
@@ -2,12 +2,17 @@ package com.bridgelabz.employeepayrollh2db;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollH2DbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollH2DbApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollH2DbApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
