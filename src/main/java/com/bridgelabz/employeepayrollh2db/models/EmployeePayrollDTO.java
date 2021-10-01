4230ae66eb0adcbbf1c55e99735b680923af428f
Prakash Goud <prakash.goud@morningstar.com>
2021-10-01 20:19:36 +0530

    [Prakash] - Added the remaining properties of payrollDTO and model

--- src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDTO.java
@@ -5,17 +5,21 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
+import lombok.ToString;
+
+
 
 public @Data class EmployeePayrollDTO {
 	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z]{2,}$", message = "Employee name Invalid")
 	public String name;
 	@Min(value = 3000, message = "Minimum wage should be greater than 3000/-")
 	public long salary;
-
-	public EmployeePayrollDTO(String name, long salary) {
+	public String gender;
+	public EmployeePayrollDTO(String name, long salary, String gender) {
 		super();
 		this.name = name;
 		this.salary = salary;
+		this.gender = gender;
 	}
 
 }
--- src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDetails.java
+++ src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDetails.java
@@ -7,13 +7,14 @@ public class EmployeePayrollDetails {
 	private int id;
 	private String name;
 	private long salary;
-	private EmployeePayrollDTO empDto;
+	private String gender;
 
 	public EmployeePayrollDetails(int id, EmployeePayrollDTO empDto) {
 		super();
 		this.id = id;
 		this.name = empDto.getName();
 		this.salary = (long) empDto.getSalary();
+		this.gender = empDto.gender;
 	}
 
 }
