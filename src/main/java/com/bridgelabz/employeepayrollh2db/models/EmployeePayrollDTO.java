ab7a0c3c54fa6d0eaf84e9d95732fff9c27526e9
Prakash Goud <prakash.goud@morningstar.com>
2021-10-01 20:56:05 +0530

    [Prakash] - Added java regex validaton for each properties

--- src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollh2db/models/EmployeePayrollDTO.java
@@ -4,8 +4,12 @@ import java.time.LocalDate;
 import java.util.List;
 
 import javax.validation.constraints.Min;
-
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Data;
 
@@ -17,14 +21,22 @@ public @Data class EmployeePayrollDTO {
 	@Min(value = 3000, message = "Minimum wage should be greater than 3000/-")
 	public long salary;
 
+	@Pattern(regexp = "male|female", message = "Gender should either male of female")
 	public String gender;
 
+
+	@JsonFormat(pattern = "dd MM yyyy")
+	@NotNull(message = "start date shouldn't be empty")
+	@PastOrPresent(message = "start date should be past or today date")	
 	public LocalDate start;
 
+	@NotBlank(message = "Note can't be empty")
 	public String note;
 
+	@NotBlank(message = "Profile picture can't be empty")
 	public String profilePic;
 
+	@NotNull(message = "Depart can't be null")
 	public List<String> department;
 
 }
